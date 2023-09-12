1e78b35977f722b70c612e0d7ad90220f96de742
Felipe Lang <felipe.lang@ext.vaadin.com>
2023-09-12 10:31:01 -0300

    fix: regression bug when synchronizing server value for date and numeric fields

--- vcf-input-mask/src/main/java/com/vaadin/componentfactory/addons/inputmask/InputMask.java
+++ vcf-input-mask/src/main/java/com/vaadin/componentfactory/addons/inputmask/InputMask.java
@@ -86,8 +86,11 @@ public class InputMask extends Component {
             if (HasValue.class.isAssignableFrom(component.getClass())) {
                 valueChangeRegistration = HasValue.class.cast(component).addValueChangeListener(e -> {
                     if (!e.isFromClient()) {
-                        getElement().executeJs("this.setValue($0)",
-                                e.getValue() == null ? "" : e.getValue().toString());
+                        if (e.getValue() == null) {
+                            getElement().executeJs("this.setValue('')");
+                        } else {
+                            getElement().executeJs("this.setValue($0.inputElement.value)", component.getElement());
+                        }
                     }
                 });
             }
