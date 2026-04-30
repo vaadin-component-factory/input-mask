22ecaabfe7f8b14cc5c954aa1979ca54d09da54d
Johannes Tuikkala <johannes@vaadin.com>
2026-04-30 17:47:46 +0300

    fixed merge issue

--- vcf-input-mask-demo/src/main/java/com/vaadin/componentfactory/demo/MainLayout.java
+++ vcf-input-mask-demo/src/main/java/com/vaadin/componentfactory/demo/MainLayout.java
@@ -31,10 +31,6 @@ public class MainLayout extends AppLayout {
         new VerticalLayout(basicUseDemo, binderWithMaskedValueDemo, binderWithUnmaskedValueDemo,
             allowWhitespaceDemo, setNewMaskDemo);
 
-    final VerticalLayout menuLayout =
-        new VerticalLayout(basicUseDemo, binderWithMaskedValueDemo, binderWithUnmaskedValueDemo,
-            allowWhitespaceDemo);
-
     addToDrawer(menuLayout);
     addToNavbar(drawerToggle);
   }
